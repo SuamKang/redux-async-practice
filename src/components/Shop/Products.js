202a22c7a0b3c16dbfc50d1e770ec8714aca7b87
Suam Kang <ksa199653@gmail.com>
2023-08-10 00:49:09 +0900

    feat: 더미 데이터 추가, 데이터 패칭 보류

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,63 +1,40 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
 import classes from "./Products.module.css";
 
 import ProductItem from "./ProductItem";
-import useFetch from "../hooks/useFetch";
-import { cartActions } from "../../store/cart-slice";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "Iphone 14 pro",
+    description: "World Best Phone",
+    price: 130,
+  },
+  {
+    id: "p2",
+    title: "Laptop",
+    description: "Wide Screen and Greate Visual",
+    price: 98,
+  },
+  {
+    id: "p3",
+    title: "Apple Watch 7",
+    description: "So Sexy Design and comfortable!!",
+    price: 50,
+  },
+];
 
 const Products = (props) => {
-  const items = useSelector((state) => state.items);
-  const dispatch = useDispatch();
-
-  const { isLoading, error, sendRequset: getRequset } = useFetch();
-
-  useEffect(() => {
-    // sendRequset effect fn
-
-    const transformItems = (itemsData) => {
-      const loadedItems = [];
-
-      for (const key in itemsData) {
-        loadedItems.push({ id: key, text: itemsData[key].items });
-      }
-
-      dispatch(cartActions.items(loadedItems));
-    };
-    getRequset(
-      {
-        url: "https://redux-async-practice-4f0bb-default-rtdb.firebaseio.com/shop.json",
-      },
-      transformItems
-    );
-  }, [getRequset]);
-
-  if (isLoading) {
-    return (
-      <section className={classes.products}>
-        <p>Loading... Please wait a secound.</p>
-      </section>
-    );
-  }
-
-  if (error)
-    return (
-      <section className={classes.products}>
-        <p>{error}</p>
-      </section>
-    );
-
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {items?.map((item) => (
+        {DUMMY_PRODUCTS?.map((item) => (
           <ProductItem
             key={item.id}
+            id={item.id}
             title={item.title}
             price={item.price}
-            description={items.description}
+            description={item.description}
           />
         ))}
       </ul>
@@ -66,3 +43,36 @@ const Products = (props) => {
 };
 
 export default Products;
+
+// useEffect(() => {
+//   const transformItems = (itemsData) => {
+//     const loadedItems = [];
+
+//     for (const key in itemsData) {
+//       loadedItems.push({ id: key, text: itemsData[key].items });
+//     }
+
+//     dispatch(cartActions.items(loadedItems));
+//   };
+//   getRequset(
+//     {
+//       url: "",
+//     },
+//     transformItems
+//   );
+// }, []);
+
+// if (isLoading) {
+//   return (
+//     <section className={classes.products}>
+//       <p>Loading... Please wait a secound.</p>
+//     </section>
+//   );
+// }
+
+// if (error)
+//   return (
+//     <section className={classes.products}>
+//       <p>{error}</p>
+//     </section>
+//   );
