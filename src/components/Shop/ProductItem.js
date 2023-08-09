b76c2f1ab39d045a888a02e33258472e37b2398b
Suam Kang <ksa199653@gmail.com>
2023-08-10 00:49:46 +0900

    feat: 장바구니 아이템 추가 헨들러 설정

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,23 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { id, title, price, description } = props;
+
+  const dispatch = useDispatch();
+
+  const addCartItemHandler = () => {
+    dispatch(
+      cartActions.addCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +28,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addCartItemHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
