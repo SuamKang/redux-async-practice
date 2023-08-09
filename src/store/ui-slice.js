2f58412edd76eace7037d4d4aa2ab7dff8a5bd0d
Suam Kang <ksa199653@gmail.com>
2023-08-10 00:47:33 +0900

    feat: uiSlice notification 프로퍼티 추가 후 리듀서 구성

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isCartVisible: false,
+  isCartVisible: false, // 장바구니 토글상태
+  notification: null, // http 요청시 알림 컴포넌트 상태 -> { status , title, message } 와 같은 프로퍼티가 담긴 객체 형태로
 };
 
 const uiSlice = createSlice({
@@ -10,6 +11,13 @@ const uiSlice = createSlice({
   reducers: {
     toggle(state) {
       state.isCartVisible = !state.isCartVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
